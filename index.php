703d85e032d8a9a8e6a6f36bf830f6ce7100bca5
Joffrey Capitaine <joffrey@yeswedev.fr>
2017-09-04 18:02:45 +0200

    twigloading

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once('models/Post.php');
-
-$loader = new Twig_Loader_Filesystem('templates'); // Dossier contenant les templates
-$twig = new Twig_Environment($loader, array(
-  'cache' => false
-));
+require_once('./vendor/autoload.php');
+require_once('./models/Post.php');
+require_once('./twigloading.php');
 
 $posts = Post::all();
 
