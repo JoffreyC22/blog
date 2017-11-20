90636806e3505c85d87750ce45e2c229ff778f80
Joffrey Capitaine <joffrey@yeswedev.fr>
2017-11-20 16:45:33 +0100

    Add post

--- post-add.php
+++ post-add.php
@@ -3,6 +3,19 @@ require_once('./vendor/autoload.php');
 require_once('./models/Post.php');
 require_once('./twigloading.php');
 
+if (!empty($_POST)){
+  $action = !empty($_GET['action']) ? $_GET['action'] : null;
+  if($action){
+      switch($action){
+          case 'save' :
+          $post = new Post();
+          $post->setTitle($_POST['title']);
+          $post->setContent($_POST['content']);
+          Post::save($post);
+      }
+  };
+}
+
 $template = $twig->loadTemplate('post-create-edit.twig');
 echo $template->render([]);
 ?>
