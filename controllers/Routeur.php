6fdeb65dc3e8952c70b2b59db5b4cb21b2883e0c
Joffrey Capitaine <joffrey@yeswedev.fr>
2017-11-29 17:48:35 +0100

    Verifications param action

--- controllers/Routeur.php
+++ controllers/Routeur.php
@@ -8,26 +8,37 @@ class Routeur{ /** Controlleur du routeur **/
 
     if (!empty($_GET['action'])) {
       $action = $_GET['action'];
-      if ($action == 'renderPost' || $action == 'editPost' || $action == 'editPostView') {
-        $post_id = $_GET['id'];
-        if (!(is_numeric($post_id)) || $post_id == '0') {
-          $error = new ErrorMessage('danger', 'Le post doit être numérique.');
-          $blog->renderError($error);
-        } else {
-          $post = Post::whereId($post_id);
-          if (!$post) {
-            $error = new ErrorMessage('danger', 'Ce post n\'existe pas.');
+      $actions = $this->getActionsPossibles('Blog');
+      if (!in_array($action, $actions)) {
+        $error = new ErrorMessage('danger', 'Cette action n\'existe pas.');
+        $blog->renderError($error);
+      } else {
+        if ($action == 'renderPost' || $action == 'editPost' || $action == 'editPostView' || $action == 'deletePost') {
+          $post_id = $_GET['id'];
+          if (!(is_numeric($post_id)) || $post_id == '0') {
+            $error = new ErrorMessage('danger', 'Le post doit être numérique.');
             $blog->renderError($error);
           } else {
-            $blog->$action();
+            $post = Post::whereId($post_id);
+            if (!$post) {
+              $error = new ErrorMessage('danger', 'Ce post n\'existe pas.');
+              $blog->renderError($error);
+            } else {
+              $blog->$action();
+            }
           }
+        } else {
+          $blog->$action();
         }
-      } else {
-        $blog->$action();
       }
     } else {
       $blog->renderHome();
     }
+  }
+
+  public function getActionsPossibles($classname){
+    $actions = get_class_methods($classname);
+    return $actions;
   }
 
 }
