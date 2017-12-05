185a39d84f815f254a17a26554e4cb427e7662a3
Joffrey Capitaine <joffrey@yeswedev.fr>
2017-12-05 12:04:55 +0100

    Refactor routeur

--- app/controllers/Routeur.php
+++ app/controllers/Routeur.php
@@ -15,46 +15,24 @@ class Routeur{ /** Controlleur du routeur **/
       $action = $_GET['action'];
       $actions = $this->getActionsPossibles('App\Controllers\Blog');
       if (!in_array($action, $actions)) {
-        $error = new ErrorMessage('danger', 'Cette action n\'existe pas.');
+        $error = new ErrorMessage('danger', 'Cette action n\'existe pas.'); /** Erreur si l'action demandée n'existe pas **/
         $blog->renderError($error);
       } else {
-        if ($this->getActionsToVerify() && !empty($_GET['id'])) {
+        if ($this->getActionsToVerify() && !empty($_GET['id'])) { /** Si il y a un id dans la requête **/
           $id = $_GET['id'];
-          $arrAction = preg_split('/(?=[A-Z])/',$action);
-          $typeId = $arrAction[1];
-          if (!(is_numeric($id)) || $id == '0') {
-            $error = new ErrorMessage('danger', 'Le '.strtolower($typeId).' doit être numérique.');
-            $blog->renderError($error);
-          } else {
-            switch ($typeId) {
-              case 'Post':
-                $class = 'App\Models\Post';
-                break;
-
-              case 'Comment':
-                $class = 'App\Models\Comment';
-                break;
-            }
-            $entity = $class::whereId($id);
-            if (!$entity) {
-              $error = new ErrorMessage('danger', 'Ce '.strtolower($typeId).' n\'existe pas.');
-              $blog->renderError($error);
-            } else {
-              $action = $_GET['action'];
-              $blog->$action();
-            }
-          }
+          $class = $this->getClass($action);
+          $this->checkIdValidity($blog, $id, $class); /** On regarde si cet id est bien numérique et qu'il correspond bien a l'entité demandée **/
         } else {
-          $action = $_GET['action'];
+          $action = $_GET['action']; /** Si pas d'id dans la requete, on renvoit directement vers l'action **/
           $blog->$action();
         }
       }
-    } else {
+    } else { /** Si pas de param action, renvoit vers la home **/
       $blog->renderHome();
     }
   }
 
-  private function getActionsPossibles($classname){ /** Retourne les actions possible pour tel controller */
+  private function getActionsPossibles($classname){ /** Retourne les actions possible pour tel controller **/
     $actions = get_class_methods($classname);
 
     return $actions;
@@ -70,6 +48,32 @@ class Routeur{ /** Controlleur du routeur **/
     }
 
     return $arrActionsToVerify;
+  }
+
+  private function getClass($action){ /** Retourne la classe correspondant a l'action (avec ou sans le namespace) **/
+    $arrAction = preg_split('/(?=[A-Z])/',$action);
+    $class = [
+      'withNamespace' => 'App\Models\\'.$arrAction[1],
+      'name' => $arrAction[1]
+    ];
+
+    return $class;
+  }
+
+  private function checkIdValidity($blog, $id, $class){
+    if (!(is_numeric($id)) || $id == '0') {
+      $error = new ErrorMessage('danger', 'Le '.strtolower($class['name']).' doit être numérique.');
+      $blog->renderError($error);
+    } else {
+      $entity = $class['withNamespace']::whereId($id);
+      if (!$entity) {
+        $error = new ErrorMessage('danger', 'Ce '.strtolower($class['name']).' n\'existe pas.');
+        $blog->renderError($error);
+      } else {
+        $action = $_GET['action'];
+        $blog->$action();
+      }
+    }
   }
 
 }
