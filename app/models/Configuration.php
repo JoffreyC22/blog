a4bcbf20f41a4b9f59c40e1fe1cb830f26c5423d
Joffrey Capitaine <joffrey@yeswedev.fr>
2017-12-22 17:05:12 +0100

    re roll config

---
+++ app/models/Configuration.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+class Configuration{
+  const DB_HOST = '127.0.0.1';
+  const DB_USER = 'root';
+  const DB_PASSWORD = 'root';
+  const DB_NAME = 'blog';
+  const BASE_URL = 'http://blog.dev/index.php?';
+}
